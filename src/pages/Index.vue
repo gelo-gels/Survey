609d6fe6acc5b8d0e4552ed5d32cb0dc353152a2
gelo-gels <riangeloo17@gmail.com>
2022-06-28 15:01:16 +0800

    deploy for publish

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,18 +2,22 @@
   <q-page class="flex flex-center bg-black">
 
 <form @submit.prevent="handleSubmit" name="contact" method="POST" data-netlify="true">
+	<q-input required type="text" name="message" v-model="form.message" borderless label="message" style="visibility: hidden"/>
     <div class="container_form">
 
     	<div class="text-bold font-poppins-bold font_sizing q-mb-lg" v-if="name_submits == true">
     		 SURVEY LANG PO ITO
     	</div>
 	    	<div class="font-poppins-bold" v-if="name_submits == true">
-	    		<q-input required type="text" name="message" v-model="form.message" borderless label="message"/>
+	    		
 
 	    		<q-input required type="text" name="name" v-model="form.name" borderless label="Please input your name" style="box-shadow: 0px 0px 10px 2px; border-radius: 10px; padding: 0px 10px" dense color="black" class="q-mb-md" />
+	    		<div class="q-mb-md text-red" v-if="error_message != ''">
+	    			Please type your name!
+	    		</div>
 		    </div>
 		    <div class="font-poppins-bold" v-if="name_submits == true">
-		    	<q-btn no-caps type="submit" label="Submit" class="btns" />
+		    	<q-btn no-caps label="Submit" class="btns" @click="click_submit()"/>
 		    </div>
 
 
@@ -23,7 +27,7 @@
     	</div>
     	<div class="row font-poppins-bold">
 	    	<div class="col-6" v-if="survey == true">
-	    		<q-btn no-caps label="Oo" class="btns" @click="click_oo()" />
+	    		<q-btn no-caps label="Oo" type="submit" class="btns" @click="click_oo()"  />
 	    	</div>
 	    	<div class="col-6 text-right" :class="survey != true ? 'visibility: hidden' : 'visibility: visible'">
 	    		<q-btn no-caps label="Wala" class="btns"  id="btn_test" />
@@ -38,8 +42,11 @@
 
 
 
-	    <div v-if="word_reveal == true" class="text-bold font-poppins-bold font_sizing q-mb-xl">
-    		YIEEE KELEG KELEG AKO BHIE HEHE SABI NA E
+	    <div v-if="word_reveal == true" class="text-bold font-poppins-bold font_sizing_reveal">
+    		YIEEE KELEG KELEG AKO BHIE AYAN HAAAA
+    	</div>
+    	<div v-if="word_reveal == true" class="text-bold font-poppins-bold font_sizing_reveal">
+    		MWAH MWA HAHSHASHASHASH
     	</div>
     </div>
 </form>
@@ -65,7 +72,7 @@ export default({
 
   data() {
   	return {
-  		
+  		error_message: '',
 
   		form: {
   			name: '',
@@ -76,16 +83,24 @@ export default({
 
   methods: {
   	async click_oo() {
-  		this.word_reveal = true
-  		this.survey = false
-  		console.log(this.name)
+  		setTimeout(()=>{
+				this.word_reveal = true
+		  		this.survey = false
+		  		console.log(this.form.name)
+			},500);
   	},
 
   	async click_submit() {
-  		this.name_submits = false
-  		this.survey = true
+  		if (this.form.name === '' || this.form.name === null || this.form.name.value === 0){
+            this.error_message = 'please type your name!'
+        }
+        else {
+        	this.name_submits = false
+  			this.survey = true
+        }
+  		
 
-		console.log(this.name)
+		console.log(this.form.name)
   	},
 
   	encode(data) {
@@ -100,7 +115,7 @@ export default({
 					'Content-type': 'application/x-www-form-urlencoded'
 				},
 				body: this.encode({
-					'form-name': 'contact',
+					'form-name': 'list of clicks',
 					...this.form
 				})
 			})
@@ -150,10 +165,14 @@ export default({
 	.container_form {
 		background: pink;
 		padding: 50px;
-		border-radius: 20px
+		border-radius: 20px;
+		margin: 0px 20px
 	}
 
 	.font_sizing {
+		font-size: 50px
+	}
+	.font_sizing_reveal {
 		font-size: 50px
 	}
 	.btns {
@@ -163,5 +182,18 @@ export default({
 		padding: 0px 20px 0px 20px;
 		border-radius: 10px;
 		box-shadow: 0px 0px 20px 5px pink;
+	}
+
+
+	@media (max-width: 646px) {
+		.font_sizing_reveal {
+			font-size: 30px
+		}
+	}
+	@media (max-width: 520px) {
+		.container_form {
+			padding: 20px;
+			margin: 0px 20px
+		}
 	}
 </style>
